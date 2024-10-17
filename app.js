8b0769d2a6eb4977889dd05b8a5eefa450761797
Sunil Park <sunilpark1129@gmail.com>
2024-10-17 10:15:28 -0400

    added env

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // 해당 path를 호출하면 indexRouter(routes)를 불러온다
 // path를 붙이는건 필수가 아니지만 좋은 습관 및 깔끔한 코딩이다
 app.use("/api", indexRouter);
 
-const mongoURI = "mongodb://localhost:27017/todo-demo";
+const mongoURI = MONGODB_URI_PROD;
 
 // useNewUrlParser - 최신 mongoose에서도 잘 호환하게 도와달라는 옵션
 mongoose
@@ -23,6 +27,6 @@ mongoose
     console.log("DB connection fail", err);
   });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server on 5000");
 });
