14509dc6435f732881b92c1a901586169aefafe8
Sunil Park <sunilpark1129@gmail.com>
2024-10-20 01:45:29 -0400

    added login, token

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -28,4 +28,24 @@ userController.createUser = async (req, res) => {
   }
 };
 
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (user) {
+      // 두개의 암호가 compareSync를 사용하여 같은지 확인하기
+      isMatch = bcrypt.compareSync(password, user.password);
+
+      if (isMatch) {
+        const token = user.generateToken();
+        res.status(200).json({ status: "success", user, token });
+      } else {
+        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
 module.exports = userController;
--- model/User.js
+++ model/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const userSchema = Schema(
   {
@@ -18,6 +21,22 @@ const userSchema = Schema(
   },
   { timestamps: true }
 );
+
+// 이 경로의 api를 사용할때 항상 이 규칙을 적용한다
+userSchema.methods.toJSON = function () {
+  // ** this를 리턴하면 스키마가 들고있는 객체를 볼 수 있다
+  // return this;
+
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+
+// 토큰 만들기
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this.id }, JWT_SECRET_KEY, { expiresIn: "1d" });
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 
